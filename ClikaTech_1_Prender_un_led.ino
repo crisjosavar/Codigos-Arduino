d6276f9bda168f3635526b2469e3e342b1ca670c
crisjosavar <cristhianvar8@gmail.com>
2024-10-23 17:16:24 -0600

    Update ClikaTech_1_Prender_un_led.ino
    arduino Uno

--- ClikaTech_1_Prender_un_led.ino
+++ ClikaTech_1_Prender_un_led.ino
@@ -1,14 +1,21 @@
-void setup() {
-  pinMode(13,OUTPUT);
-  
 
+int i=5;
+
+vois setuo() {
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT)
+  pinMode(7,OUTPUT)
+  pinMode(8,OUTPUT)
+  pinMode(9,OUTPUT)
+  pinMode(10,OUTPUT)
+  pinMode(11,OUTPUT)
+  
 }
-
 void loop() {
-  digitalWrite(13,HIGH);
-  delay(100);
-  digitalWrite(13,LOW);
-  delay(100);
-  
+  for(i=5; i<=11; i++){
+  digitalWrite(i,HIGH);
+  digitalWrite(i-1,LOW);
+  delay(500);
+  } 
 
 }
